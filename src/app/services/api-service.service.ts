abb2fd9efb745c3813f6f6a81ac87ebae5e9d0a9
m.krsmanovic <m.krsmanovic@fuentis.com>
2024-01-07 20:27:04 +0100

    adding enviroments

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,45 +1,45 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { SseServiceService } from './sse-service.service';
 import { Observable } from 'rxjs';
 import { TranslationItem } from '../utils/translation-item.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServiceService {
-  constructor(private http: HttpClient, private sse: SseServiceService) {}
+  constructor(private http: HttpClient, private sse: SseServiceService) {
+    if (isDevMode()) {
+      console.log('running in DEV');
+    } else {
+      console.log('running in PROD');
+    }
+
+    console.log(environment.apiUrl);
+  }
 
   getAllTasks(query?: string) {
     let params;
     if (query) {
       params = new HttpParams().set('missing', query);
     }
-
-    return this.http.get<TranslationItem[]>(
-      'https://i18n-hub-62d28556e54a.herokuapp.com/task',
-      {
-        params,
-      }
-    );
+    return this.http.get<TranslationItem[]>(`${environment.apiUrl}/task`, {
+      params,
+    });
   }
 
   deleteItem(id: string) {
-    return this.http.delete(
-      'https://i18n-hub-62d28556e54a.herokuapp.com/task/' + id
-    );
+    return this.http.delete(`${environment.apiUrl}/task/${id}`);
   }
 
   createSingleTask(body: TranslationItem) {
-    return this.http.post<TranslationItem>(
-      'https://i18n-hub-62d28556e54a.herokuapp.com/task',
-      body
-    );
+    return this.http.post<TranslationItem>(`${environment.apiUrl}/task`, body);
   }
 
   updateTranslationObj(id: string, body: any) {
     return this.http.patch<TranslationItem>(
-      'https://i18n-hub-62d28556e54a.herokuapp.com/task' + id,
+      `${environment.apiUrl}/task/${id}`,
       body
     );
   }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://i18n-hub-62d28556e54a.herokuapp.com',
+};
